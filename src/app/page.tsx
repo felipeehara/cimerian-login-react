8fa5a09e3860396954ea88d6b4565f14a19b46d6
Felipe Ehara <113571580+felipeehara@users.noreply.github.com>
2024-10-02 19:54:47 -0300

    Update page.tsx
    Arrumando erro de usuário

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
           <img className="w-80 md:w-96 lg:w-1/2 xl:w-96  2xl:max-w-5xl" src="/img/logo_cimerianeditado.png" alt="Logo"></img>
           </div>
           <div className="flex flex-col justify-center items-center mt-8 xl:mb-36 2xl:mb-96">
-          <input className="w-80 mb-5 border-4 border-white text-xl focus:border-gray-500 focus:outline-none rounded-md md:w-96 lg:w-1/2 lg:text-3xl xl:m-1 xl:h-10 xl:w-96 xl:text-xl xl:bg-gray-300 xl:border-gray-300" type="email" placeholder="Usúario"></input>
+          <input className="w-80 mb-5 border-4 border-white text-xl focus:border-gray-500 focus:outline-none rounded-md md:w-96 lg:w-1/2 lg:text-3xl xl:m-1 xl:h-10 xl:w-96 xl:text-xl xl:bg-gray-300 xl:border-gray-300" type="email" placeholder="Usuário"></input>
           <input className="w-80 mb-5 border-4 border-white text-xl rounded-md focus:border-gray-500 focus:outline-none md:w-96 lg:w-1/2 lg:text-3xl xl:m-1 xl:h-10 xl:w-96 xl:text-xl xl:bg-gray-300 xl:border-gray-300" type="password" placeholder="Senha"></input>
           <button className="px-12 py-2 bg-gray-300 rounded-md font-bold text-xl active:bg-gray-400 md:py-3 md:px-14 lg:py-5 lg:px-16 lg:text-3xl xl:w-28 xl:h-10 xl:py-0 xl:px-0 xl:text-xl">Entrar</button>
           </div>
