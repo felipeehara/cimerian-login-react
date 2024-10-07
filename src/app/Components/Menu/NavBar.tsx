2d416a881e3e8afd313e4e2595f956f8f351eb4e
Felipe Ehara <felipeyoug1@gmail.com>
2024-10-07 15:51:28 -0300

    arrumando pastas SVGS

--- src/app/Components/Menu/NavBar.tsx
+++ src/app/Components/Menu/NavBar.tsx
@@ -5,8 +5,8 @@ import Link from "next/link";
 import Image from "next/image";
 import { usePathname } from "next/navigation";
 import imgLogo from "/public/img/logo_cimerianeditado.png";
-import iconHome from "/public/svgs/IconHome.svg";
-import iconProfile from "/public/svgs/profile/profile.svg";
+import iconHome from "/public/Svgs/IconHome.svg";
+import iconProfile from "/public/Svgs/profile/profile.svg";
 
 import { Profile, ProfileMobile } from '@/app/Components/Profile/Profile';
 
