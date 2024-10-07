a840a5b9a05b043cca8c8a7c4a1fdbc119ab2853
Felipe Ehara <felipeyoug1@gmail.com>
2024-10-07 15:31:29 -0300

    implementação de text no Home

--- src/app/Home/page.tsx
+++ src/app/Home/page.tsx
@@ -8,8 +8,12 @@ const Home = () => {
             <Header/>
 
 
-            <h1 className="ml-10 mt-10">Olá, Felipe Ehara. <br/>
-            Que bom te ver por aqui! :)</h1>
+            <div className="ml-10 mt-10">   
+                <p className="flex gap-1"> <b>Olá,</b> <h1 className="text-blue-600 font-bold">Felipe Ehara</h1> </p>  
+                <p>Que bom te ver por aqui! :)</p>
+            </div>
+
+            
         </div>
     );
 }
